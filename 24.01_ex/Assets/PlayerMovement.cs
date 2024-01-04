113bd5b524625466dacbdbbdf241d61d018ddafd
zion <sangwook4859@naver.com>
2024-01-04 12:39:54 +0900

    24.01.03

---
+++ 24.01_ex/Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerController inputActions;
+
+    private void OnEnable()
+    {
+
+    }
+    private void OnDisable()
+    {
+        
+    }
+}
